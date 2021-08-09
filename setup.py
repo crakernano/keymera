cddccae1b5c3185395382bb940400718864c9660
Trastejant <nano@trastejant.com>
2021-08-10 00:45:54 +0200

    fichero de setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='keymera',
+    packages=['keymera'],
+    version='0.0.0-alpha',
+    licence='MIT',
+    description='ethical hacking suite',
+    authon='CrakerNano',
+    author_email='',
+    url='',
+    downloard_url='',
+    keywords='hacking, scanner',
+    python_requires='>=2.7',
+    classifiers=['Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.6.9'
+                 ],
+)
