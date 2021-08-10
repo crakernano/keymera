1ab60ab139054e8f65c95d3e5ef1d7e0137ff169
Trastejant <nano@trastejant.com>
2021-08-10 20:02:45 +0200

    feature(script):add test

---
+++ test_help.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import main
+
+
+class TestTheHelp(unittest.TestCase):
+
+    def test_help(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
